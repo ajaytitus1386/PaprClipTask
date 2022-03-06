0c4c235e19d20e09aa6bc1b13718aab9b8e2ab41
Ajay Titus <ajaytitus1386@gmail.com>
2022-03-06 13:51:51 +0530

    style: colors and text styles

---
+++ lib/presentation/styling/text_styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+TextStyle buildPageHeadingStyle() {
+  return const TextStyle(
+      color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold);
+}
+
+TextStyle buildDefaultStyle() {
+  return const TextStyle(
+      color: Colors.white, fontSize: 14, fontWeight: FontWeight.w600);
+}
+
+TextStyle buildBoldNormalStyle() {
+  return const TextStyle(
+      color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold);
+}
