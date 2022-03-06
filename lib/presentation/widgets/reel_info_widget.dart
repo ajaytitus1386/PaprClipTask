103c384c63f75240734a1dd50661b543ba7d8d7e
Ajay Titus <ajaytitus1386@gmail.com>
2022-03-06 13:52:51 +0530

    feat: reel info bar

---
+++ lib/presentation/widgets/reel_info_widget.dart
@@ -0,0 +1,40 @@
+import 'package:expandable_text/expandable_text.dart';
+import 'package:flutter/material.dart';
+import 'package:papr_clip_task/domain/entities/reel.dart';
+import 'package:papr_clip_task/presentation/styling/text_styles.dart';
+
+class ReelInfoWidget extends StatelessWidget {
+  final Reel reel;
+  const ReelInfoWidget({Key? key, required this.reel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text(
+            reel.sourcePage,
+            style: buildBoldNormalStyle(),
+          ),
+          leading: CircleAvatar(
+            radius: 20,
+            backgroundImage: NetworkImage(reel.displayImage),
+          ),
+        ),
+        ExpandableText(
+          reel.title,
+          expandText: '',
+          expandOnTextTap: true,
+          collapseOnTextTap: true,
+          maxLines: 1,
+          style: buildDefaultStyle(),
+          linkColor: Colors.white,
+          textAlign: TextAlign.left,
+        ),
+        const SizedBox(
+          height: 20,
+        )
+      ],
+    );
+  }
+}
