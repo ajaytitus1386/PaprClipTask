4c0dae186e31cd15872c16c74fd39635e886ee2f
Ajay Titus <ajaytitus1386@gmail.com>
2022-03-07 00:53:13 +0530

    feat: add updating like icon

--- lib/presentation/widgets/reel_actions_widget.dart
+++ lib/presentation/widgets/reel_actions_widget.dart
@@ -5,28 +5,49 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:papr_clip_task/presentation/styling/text_styles.dart';
 
 class ReelActionsWidget extends StatefulWidget {
-  const ReelActionsWidget({Key? key}) : super(key: key);
+  late bool isLiked;
+  ReelActionsWidget({Key? key, required this.isLiked}) : super(key: key);
 
   @override
   State<ReelActionsWidget> createState() => _ReelActionsWidgetState();
 }
 
 class _ReelActionsWidgetState extends State<ReelActionsWidget> {
+  // Randomly Generated Like and Comment counts
   int likes = Random().nextInt(899) + 100;
   int comments = Random().nextInt(5999) + 1000;
   @override
   Widget build(BuildContext context) {
     const double iconSize = 30.0;
+
+    void toggleLike() {
+      setState(() {
+        widget.isLiked = !widget.isLiked;
+      });
+    }
+
+    Widget likeIconOutlinedOrSolid = widget.isLiked
+        // If is liked, use solid icon
+        ? FaIcon(
+            FontAwesomeIcons.solidHeart,
+            size: iconSize,
+            color: Colors.red.shade400,
+          )
+        : const FaIcon(
+            FontAwesomeIcons.heart,
+            size: iconSize,
+            color: Colors.white,
+          );
+
     return SizedBox(
       height: 300,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           buildSideIconButton(
-              icon: const FaIcon(
-            FontAwesomeIcons.heart,
-            size: iconSize,
-          )),
+            icon: likeIconOutlinedOrSolid,
+            onTap: toggleLike,
+          ),
           Text(
             likes.toString() + 'k',
             style: buildDefaultStyle(),
