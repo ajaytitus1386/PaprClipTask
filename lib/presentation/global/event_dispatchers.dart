2312f90a753d0b182894e6b8fdee729b8a25d79a
Ajay Titus <ajaytitus1386@gmail.com>
2022-03-07 15:56:25 +0530

    docs: add doc comments

--- lib/presentation/global/event_dispatchers.dart
+++ lib/presentation/global/event_dispatchers.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:papr_clip_task/presentation/bloc/reel_bloc.dart';
 
+/// When called, notifies BlocBuilder that state has changed and triggers the [GetAllReelsFromApiEvent]
 void dispatchGetAllReels(BuildContext context) {
   BlocProvider.of<ReelBloc>(context).add(GetAllReelsFromApiEvent());
 }
