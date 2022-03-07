33e83354698836758b26203ae3ad283fdb392ef4
Ajay Titus <ajaytitus1386@gmail.com>
2022-03-07 08:54:59 +0530

    style: splash screen gradient

--- lib/presentation/styling/color_palettes.dart
+++ lib/presentation/styling/color_palettes.dart
@@ -5,3 +5,9 @@ LinearGradient reelTopGradient = const LinearGradient(
     end: Alignment.bottomCenter,
     colors: [Colors.black, Colors.transparent],
     stops: [0.5]);
+
+RadialGradient splashScreenGradient = RadialGradient(
+  colors: [Colors.black, Colors.black.withOpacity(0.85)],
+  stops: const [0.1, 0.9],
+  radius: 1.5,
+);
